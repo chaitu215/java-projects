148b2893950c5f1163546939c93c34e95c53c821
wsy858 <wsy858@126.com>
2017-02-24 18:35:59 +0800

    update

---
+++ grammar/src/evan/wang/text/TestMatcherReplace.java
@@ -0,0 +1,36 @@
+package evan.wang.text;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * 正则提取并替换
+ * @author: wangsy
+ * @date: 2017年2月21日
+ */
+public class TestMatcherReplace {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		String sql = "select  t.[symbol], cast(wer.[ymd] as int) + 1 as wert, [price_open], [price_high], [price_low], [price_close], [volumn], [price_adj_close] from  [CSV股票3333] t where [price_open] > 2.3";
+		Pattern pattern = Pattern.compile("([\\w]+\\.)?\\[[^\\[\\]]*\\]"); //t.[name]或者 [name]
+		Matcher matcher = pattern.matcher(sql);
+		int i=0;
+		StringBuffer sb = new StringBuffer();
+		while (matcher.find()) {
+			String string = matcher.group(0);
+			matcher.appendReplacement(sb, "rep"+i); //替换
+			if (string != null && !"".equals(string)){
+				System.out.println(matcher.group(0));
+			}
+			i++;
+		}
+		matcher.appendTail(sb); 
+		System.out.println(sb.toString());
+		System.out.println("总数: " + i);
+
+	}
+
+}
